2dcec7d56fc37f1a71f170ed45456d041116239a
Rafael Padron <rafael.padron@gmail.com>
2020-12-08 11:26:13 -0400

    se creo libreria core para nestjs

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  projects: ['<rootDir>/apps/website', '<rootDir>/libs/ui', '<rootDir>/apps/chat', '<rootDir>/apps/api'],
+  projects: [
+    '<rootDir>/apps/website',
+    '<rootDir>/libs/ui',
+    '<rootDir>/apps/chat',
+    '<rootDir>/apps/api',
+    '<rootDir>/libs/core',
+  ],
 }
