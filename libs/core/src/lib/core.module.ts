94487261f74a38b43759d8154cbcf466aa6eec09
Rafael Padron <rafael.padron@gmail.com>
2020-12-08 13:00:01 -0400

    feat: NesJS API y Core lirary, enviroment config

--- libs/core/src/lib/core.module.ts
+++ libs/core/src/lib/core.module.ts
@@ -1,6 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { configuration } from './config/configurarion'
+import { validationSchema } from './config/validation'
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      validationSchema,
+    }),
+  ],
   controllers: [],
   providers: [],
   exports: [],
